5b718b469a36956445e94523d55fcbbea30ac308
BillyZeBlack <34786459+BillyZeBlack@users.noreply.github.com>
2022-07-29 18:29:13 +0200

    Initial Commit

---
+++ Mon lieux de culte/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mon lieux de culte
+//
+//  Created by williams saadi on 29/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mon lieux de culte/Mon_lieux_de_culteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mon_lieux_de_culteApp.swift
+//  Mon lieux de culte
+//
+//  Created by williams saadi on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Mon_lieux_de_culteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
