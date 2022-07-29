e1a7c6377349887a0c958f0f9976823ebdf31914
BillyZeBlack <34786459+BillyZeBlack@users.noreply.github.com>
2022-07-29 18:52:01 +0200

    add to first commit

---
+++ Mon lieux de culte/Model/Comment.swift
@@ -0,0 +1,22 @@
+//
+//  Comment.swift
+//  Mon lieux de culte
+//
+//  Created by williams saadi on 29/07/2022.
+//
+
+import Foundation
+
+class Comment {
+    
+    var title: String
+    var content: String
+    var author: String
+    
+    init(title: String, content: String, author: String) {
+        self.title = title
+        self.content = content
+        self.author = author
+    }
+}
+
