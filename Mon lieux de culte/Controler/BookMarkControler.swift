6800d2774e60cee64fd50f2859e6337ec59d67a1
BillyZeBlack <34786459+BillyZeBlack@users.noreply.github.com>
2022-07-30 09:58:09 +0200

    - AJOUT : BookmarkController file

---
+++ Mon lieux de culte/Controler/BookMarkControler.swift
@@ -0,0 +1,23 @@
+//
+//  BookMarkControler.swift
+//  Mon lieux de culte
+//
+//  Created by williams saadi on 30/07/2022.
+//
+
+import Foundation
+
+class BookmarkControler {
+    
+    var bookmarksList: [Bookmark] = []
+    
+    public func addBookmarkIntoList(bookmark: Bookmark)
+    {
+        
+    }
+    
+    public func removeBookmarkFromList(bookmark: Bookmark)
+    {
+        
+    }
+}
